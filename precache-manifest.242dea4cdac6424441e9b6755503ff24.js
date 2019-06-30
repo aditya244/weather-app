671eb376ebfd27cbf4c60063733726d83c2a4b6b
unknown <sinha.aditya244@gmail.com>
2019-07-01 00:18:21 +0530

    Updates

---
+++ precache-manifest.242dea4cdac6424441e9b6755503ff24.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0f969aa5fbf54cc3be7afa5a3325a9c",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "bddda4bc3dabaa4dacbc",
+    "url": "/weather-app/static/css/main.a9efdcd7.chunk.css"
+  },
+  {
+    "revision": "0f96490e3eb2cb3e2448",
+    "url": "/weather-app/static/js/2.ec057f4a.chunk.js"
+  },
+  {
+    "revision": "bddda4bc3dabaa4dacbc",
+    "url": "/weather-app/static/js/main.3e4d3df4.chunk.js"
+  },
+  {
+    "revision": "90e6a6c358468ae00209",
+    "url": "/weather-app/static/js/runtime~main.4606d7f5.js"
+  }
+]);
